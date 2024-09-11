ad4945799e3cc1330904c79f6ea218053935c8e4
mutabazichristian <c.mutabazi@alustudent.com>
2024-09-11 15:59:47 +0200

    task 2

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""A function that returns the transpose of a 2D matrix"""
+
+
+def matrix_transpose(matrix):
+    print("tryna find a matrix transpose huh?")
+    print(matrix)
+
+
+mat1 = [[1, 2], [3, 4]]
+print(mat1)
+print(matrix_transpose(mat1))
