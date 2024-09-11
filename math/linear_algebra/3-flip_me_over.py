10ac5e86b26653cbaa8e7a1a1a54f361512d1adf
mutabazichristian <c.mutabazi@alustudent.com>
2024-09-11 16:44:05 +0200

    task 3

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """A function that returns the transpose of a 2D matrix"""
+import numpy as np
 
 
 def matrix_transpose(matrix):
@@ -7,14 +8,21 @@ def matrix_transpose(matrix):
     Arguements: A matrix
     Returns: A transpose of that matrix
     """
-    transpose = []
-    rows = len(matrix)
-    cols = len(matrix[0])
-    # iterate through the rows
-    for i in range(rows):
-        row = []
-        # iterate through the columns
-        for j in range(cols):
-            row.append(matrix[j][i])
-        transpose.append(row)
+    matrix_numpy = np.array(matrix)
+    transpose = np.transpose(matrix_numpy)
     return transpose
+
+
+print(matrix_transpose([[1, 2], [3, 4]]))
+print(
+    matrix_transpose(
+        [
+            [1, 2, 3, 4, 5],
+            [6, 7, 8, 9, 10],
+            [11, 12, 13, 14, 15],
+            [16, 17, 18, 19, 20],
+            [21, 22, 23, 24, 25],
+            [26, 27, 28, 29, 30],
+        ]
+    )
+)
