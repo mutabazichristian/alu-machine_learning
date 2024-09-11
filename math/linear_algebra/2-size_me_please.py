f00b5d4227b39eeac7e2a26f45c133478f2e46b2
mutabazichristian <c.mutabazi@alustudent.com>
2024-09-11 10:18:27 +0200

    task 2

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
+    """Function that return the shape of matrix, all dimensions"""
     shape = []
     while isinstance(matrix, list):
         shape.append((len(matrix)))
         matrix = matrix[0]
 
     return shape
-
