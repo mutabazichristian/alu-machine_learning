6dce4f7d41d0c3f9c2f0cbbbb01576e1fb9b2668
mutabazichristian <c.mutabazi@alustudent.com>
2024-09-12 11:43:17 +0200

    task 4

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -3,6 +3,10 @@
 
 
 def add_matrices2D(mat1, mat2):
+    """
+    arguments: 2 matrices
+    returns: the addition matrix of the 2 matrices
+    """
     sum_mat = []
     if (len(mat1) != len(mat2)) or (len(mat1[0]) != len(mat2)):
         return None
@@ -13,3 +17,6 @@ def add_matrices2D(mat1, mat2):
             row.append(mat1[i][j] + mat2[i][j])
         sum_mat.append(row)
     return sum_mat
+
+
+print(add_matrices2D([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
