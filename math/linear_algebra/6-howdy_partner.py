0fdaab20e9db879ca4e02ba088f3469b88fd3eef
mutabazichristian <c.mutabazi@alustudent.com>
2024-09-12 12:00:54 +0200

    task 6

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Function that concatenates 2 arrays"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Arguments: 2 arrays
+    Returns: one array of the concatenated 2 arrays
+    """
+    return arr1 + arr2
+
+
+print(cat_arrays([1, 2], [3, 4]))
