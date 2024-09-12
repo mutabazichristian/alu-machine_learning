67e1ba6c319d532f57a4a0826f2901f7f49a68bf
mutabazichristian <c.mutabazi@alustudent.com>
2024-09-12 11:21:22 +0200

    task 3

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Function that performs addition of elements of 2 arrays"""
+
+
+def add_arrays(arr1, arr2):
+    """
+    Arguments: 2 arrays
+    Returns: A new array
+    """
+    if len(arr1) != len(arr2):
+        return None
+    arr_sum = []
+    for i in range(len(arr2)):
+        arr_sum = arr1[i] + arr2[i]
+    return arr_sum
+
+
+print(add_arrays([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]))
