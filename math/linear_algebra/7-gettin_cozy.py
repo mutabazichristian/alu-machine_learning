6530a483e4ee946c563f9344b9d3020030e873f6
mutabazichristian <c.mutabazi@alustudent.com>
2024-09-12 12:23:19 +0200

    task 6

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Function that concatenate to matrices along a specific axis"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """
+    Arguments: 2 matrices, axis to perfom addition upon
+    Returns:
+    the concatenated matrix
+    None if they can't be concatenated
+    """
+    if axis == 0:
+        return mat1 + mat2
+    if axis == 1:
+        concat_mat = []
+        # check if mat 1 and 2 are the same shape
+        if len(mat1) != len(mat2):
+            return None
+        for i in range(mat2):
+            mat1[i].append(mat2[i])
+        return concat_mat
+    else:
+        return None
