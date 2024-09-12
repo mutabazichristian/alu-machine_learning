2ef776dcbd82b4b48d3eb1f884526c9d800657e7
mutabazichristian <c.mutabazi@alustudent.com>
2024-09-12 14:21:00 +0200

    task 7

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -1,38 +1,18 @@
-#!/usr/bin/env python3
-"""Function that concatenate to matrices along a specific axis"""
-
-
 def cat_matrices2D(mat1, mat2, axis=0):
-    """
-    Arguments: 2 matrices, axis to perfom addition upon
-    Returns:
-    the concatenated matrix
-    None if they can't be concatenated
-    """
+    # Ensure the matrices can be concatenated
     if axis == 0:
-        return mat1 + mat2
-    if axis == 1:
-        concat_mat = []
-        # check if mat 1 and 2 are the same shape
+        # Check if the number of columns in both matrices is the same
+        if len(mat1[0]) != len(mat2[0]):
+            return None
+        # Return a new matrix with rows from both matrices
+        return [row[:] for row in mat1] + [row[:] for row in mat2]
+
+    elif axis == 1:
+        # Check if the number of rows in both matrices is the same
         if len(mat1) != len(mat2):
             return None
-        for i in range(len(mat2)):
-            mat1[i].append(mat2[i])
-        concat_mat = mat1
-        return concat_mat
-    else:
-        return None
+        # Concatenate corresponding rows from both matrices
+        return [mat1[i] + mat2[i] for i in range(len(mat1))]
 
-
-mat1 = [[1, 2], [3, 4]]
-mat2 = [[5, 6]]
-mat3 = [[7], [8]]
-mat4 = cat_matrices2D(mat1, mat2)
-mat5 = cat_matrices2D(mat1, mat3, axis=1)
-print(mat4)
-print(mat5)
-mat1[0] = [9, 10]
-mat1[1].append(5)
-print(mat1)
-print(mat4)
-print(mat5)
+    # If the axis is not 0 or 1, return None
+    return None
