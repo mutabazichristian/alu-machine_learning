08481bff0826c8b7e9f15cdc523e082c7af0b801
mutabazichristian <c.mutabazi@alustudent.com>
2024-09-12 11:31:36 +0200

    task 4

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""A function that add 2 matricess together"""
+
+
+def add_matrices2D(mat1, mat2):
+    sum_mat = []
+    if (len(mat1) != len(mat2)) or (len(mat1[0]) != len(mat2)):
+        return None
+    # loop through row
+    for i in range(len(mat2)):
+        row = []
+        for j in range(len(mat2[0])):
+            row.append(mat1[i][j] + mat2[i][j])
+        sum_mat.append(row)
+    return sum_mat
+
+
+print(add_matrices2D([[1, 2], [3, 4]], [[1, 2], [3, 4, 5]]))
