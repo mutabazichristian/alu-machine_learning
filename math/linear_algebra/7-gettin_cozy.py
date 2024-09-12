bc4a31e2654c7c1797f53a7284bd5ed326acc9ce
mutabazichristian <c.mutabazi@alustudent.com>
2024-09-12 14:07:50 +0200

    task 7

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -22,3 +22,17 @@ def cat_matrices2D(mat1, mat2, axis=0):
         return concat_mat
     else:
         return None
+
+
+mat1 = [[1, 2], [3, 4]]
+mat2 = [[5, 6]]
+mat3 = [[7], [8]]
+mat4 = cat_matrices2D(mat1, mat2)
+mat5 = cat_matrices2D(mat1, mat3, axis=1)
+print(mat4)
+print(mat5)
+mat1[0] = [9, 10]
+mat1[1].append(5)
+print(mat1)
+print(mat4)
+print(mat5)
