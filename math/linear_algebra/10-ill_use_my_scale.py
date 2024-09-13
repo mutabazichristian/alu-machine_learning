d1bc32a32a1af92be78dde8cac0ffdae0d5d837b
mutabazichristian <c.mutabazi@alustudent.com>
2024-09-13 11:49:41 +0200

    task 10

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """Function that calculates the shape of an array"""
-import numpy as np
 
 
 def np_shape(matrix):
@@ -12,13 +11,3 @@ def np_shape(matrix):
     A tuple of integers representing the shape of the given matrix
     """
     return matrix.shape
-
-
-mat1 = np.array([1, 2, 3, 4, 5, 6])
-mat2 = np.array([])
-mat3 = np.array(
-    [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]], [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]]
-)
-print(np_shape(mat1))
-print(np_shape(mat2))
-print(np_shape(mat3))
