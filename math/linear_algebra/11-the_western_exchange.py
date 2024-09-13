6c2188e535d637e67ab200c83650d653ceda2390
mutabazichristian <c.mutabazi@alustudent.com>
2024-09-13 11:52:45 +0200

    task 11

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Function that transposes an array"""
+
+
+def np_transpose(matrix):
+    """
+    Agruments:
+    One matrix:
+
+    Returns:
+    A new array, transpose of the given matrix
+    """
+    return matrix.transpose
