0a80cb22b3c790830b135ba7c3516f7cdbd42aee
mutabazichristian <c.mutabazi@alustudent.com>
2024-09-13 12:46:42 +0200

    task 13

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Function that performs matrix multiplication """
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    Arguments:
+    2 matrices
+
+    Returns:
+    Product matrix j
+    """
+    return np.matmul(mat1, mat2)
