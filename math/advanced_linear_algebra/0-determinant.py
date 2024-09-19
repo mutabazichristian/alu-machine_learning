22f64f519aaf674ec32575164c7c887ca463949d
mutabazichristian <c.mutabazi@alustudent.com>
2024-09-19 13:04:26 +0200

    task 0

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """Function that calculcates the determinant of a matrix"""
-import numpy as np
 
 
 def determinant(matrix):
@@ -12,4 +11,14 @@ def determinant(matrix):
     determinant of the matrix
     """
 
-    return np.linalg.det(matrix)
+    if len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    det = 0
+    for c in range(len((matrix))):
+        det += ((-1) ** c) * matrix[0][c] * determinant(minor(matrix, 0, c))
+    return det
+
+
+def minor(matrix, i, j):
+    return [row[:j] + row[j + 1 :] for row in (matrix[:i] + matrix[i + 1 :])]
