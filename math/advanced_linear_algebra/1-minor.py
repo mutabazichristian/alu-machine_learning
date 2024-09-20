7fd62024c8f995fda78d288157d61c5ff49f16f0
mutabazichristian <c.mutabazi@alustudent.com>
2024-09-20 12:58:05 +0200

    task 1

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -1,3 +1,25 @@
-import numpy as np
-x=np.array([1,2])
-print(x)
+#!/usr/bin/env python3
+"""Function that calculate the minor of a matrix"""
+
+
+def minor(matrix):
+    """
+    Args:
+    matrix: A square matrix (list of lists)
+
+    Returns:
+    The minor of the matrix or raises an error for invalid input
+    """
+
+    # Check if the matrix is empty
+    if matrix == [[]]:  # Handle empty matrix [[]] as a special case
+        return 1
+
+    # Check if the matrix is square
+    if len(matrix) != len(matrix[0]):
+        raise ValueError("matrix must be a square matrix")
+
+    # Handle 1x1 matrix case
+    if len(matrix) == 1:
+        return matrix[0][0]
+    return [row[:j] + row[j + 1:] for row in (matrix[:i] + matrix[i + 1:])]
