5606b92b163d786886d2aef11872ed80c0d010c6
mutabazichristian <c.mutabazi@alustudent.com>
2024-09-20 11:32:01 +0200

    task 0

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -31,25 +31,3 @@ def determinant(matrix):
 
 def minor(matrix, i, j):
     return [row[:j] + row[j + 1 :] for row in (matrix[:i] + matrix[i + 1 :])]
-
-
-mat0 = [[]]
-mat1 = [[5]]
-mat2 = [[1, 2], [3, 4]]
-mat3 = [[1, 1], [1, 1]]
-mat4 = [[5, 7, 9], [3, 1, 8], [6, 2, 4]]
-mat5 = []
-mat6 = [[1, 2, 3], [4, 5, 6]]
-print(determinant(mat0))
-print(determinant(mat1))
-print(determinant(mat2))
-print(determinant(mat3))
-print(determinant(mat4))
-try:
-    determinant(mat5)
-except Exception as e:
-    print(e)
-try:
-    determinant(mat6)
-except Exception as e:
-    print(e)
