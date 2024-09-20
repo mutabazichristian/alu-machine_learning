446ea4753d63f5c446f3ef5393671908877d351b
mutabazichristian <c.mutabazi@alustudent.com>
2024-09-20 11:18:29 +0200

    task 0

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -10,10 +10,16 @@ def determinant(matrix):
     Returns:
     determinant of the matrix
     """
-    if not (isinstance(matrix, list) and isinstance(matrix[0], list)):
+    try:
+        if not (isinstance(matrix, list) and isinstance(matrix[0], list)):
+            return "matrix should be a list"
+    except Exception as e:
         return "matrix should be a list"
     if len(matrix) != len(matrix[0]):
         return "matrix should be a square matrix"
+    if len(matrix) == 1:
+        return matrix[0][0]
+
     if len(matrix) == 2:
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
 
@@ -25,3 +31,25 @@ def determinant(matrix):
 
 def minor(matrix, i, j):
     return [row[:j] + row[j + 1 :] for row in (matrix[:i] + matrix[i + 1 :])]
+
+
+mat0 = [[]]
+mat1 = [[5]]
+mat2 = [[1, 2], [3, 4]]
+mat3 = [[1, 1], [1, 1]]
+mat4 = [[5, 7, 9], [3, 1, 8], [6, 2, 4]]
+mat5 = []
+mat6 = [[1, 2, 3], [4, 5, 6]]
+print(determinant(mat0))
+print(determinant(mat1))
+print(determinant(mat2))
+print(determinant(mat3))
+print(determinant(mat4))
+try:
+    determinant(mat5)
+except Exception as e:
+    print(e)
+try:
+    determinant(mat6)
+except Exception as e:
+    print(e)
