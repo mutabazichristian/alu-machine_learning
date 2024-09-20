32822eac918776a00c0c339c9ac224a0ce161b57
mutabazichristian <c.mutabazi@alustudent.com>
2024-09-20 14:32:09 +0200

    task 2

--- math/advanced_linear_algebra/2-cofactor.py
+++ math/advanced_linear_algebra/2-cofactor.py
@@ -2,14 +2,33 @@
 """ Function that calculates the cofactor of a matrix"""
 import numpy as np
 
+def determinant(matrix):
+    """
+    Args:
+    matrix
+
+    Returns:
+    determinant
+    """
+    n = len(matrix)
+    if n == 1:
+        return matrix[0][0]
+    if n == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    
+    det = 0
+    for j in range(n):
+        sub_matrix = [row[:j] + row[j+1:] for row in matrix[1:]]
+        det += ((-1) ** j) * matrix[0][j] * determinant(sub_matrix)
+    return det
 
 def cofactor(matrix):
     """
     Args:
-    matrix: some matrix
+    matrix
 
     Returns:
-    cofactor matrix
+    matrix
     """
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
@@ -27,7 +46,7 @@ def cofactor(matrix):
         cofactor_row = []
         for j in range(n):
             minor = [row[:j] + row[j+1:] for row in (matrix[:i] + matrix[i+1:])]
-            cofactor_row.append(((-1) ** (i + j)) * int(round(np.linalg.det(minor))))
+            cofactor_row.append(((-1) ** (i + j)) * determinant(minor))
         cofactor_matrix.append(cofactor_row)
     
     return cofactor_matrix
