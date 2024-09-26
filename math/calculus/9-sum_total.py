ebc433060336bb651974a7cb26dee73754dc51ee
mutabazichristian <c.mutabazi@alustudent.com>
2024-09-26 14:06:21 +0200

    task 8

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+Function to Calculate the give intranet summation
+"""
+def summation_i_squared(n):
+    if not isinstance(n,(int,float)):
+        return None  
+    return 
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * n * factorial(n-1)
