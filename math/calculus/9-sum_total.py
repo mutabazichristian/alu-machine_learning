9fb4f11ef5e3fae8399718271560ac5a08974501
mutabazichristian <c.mutabazi@alustudent.com>
2024-09-26 15:07:22 +0200

    task 9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -5,6 +5,14 @@ Function to Calculate the give intranet summation
 
 
 def summation_i_squared(n):
+    """
+    Argument:
+    int to be squared as per formula
+
+    Return:
+    None if not real integer
+    the results' int
+    """
     if not isinstance(n, int) or n < 1:
         return None
     return (n * (n + 1) * (2 * n + 1)) // 6
