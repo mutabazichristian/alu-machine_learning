1023adccb933fa47e1b48c2bdd95efd076804bec
mutabazichristian <c.mutabazi@alustudent.com>
2024-09-26 14:12:12 +0200

    task 8

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -3,12 +3,6 @@
 Function to Calculate the give intranet summation
 """
 def summation_i_squared(n):
-    if not isinstance(n,(int,float)):
-        return None  
-    return factorial(n)
-
-def factorial(a):
-    if a == 0 or a == 1:
-        return 1
-    else:
-        return a * a * factorial(a-1)
+    if not isinstance(n, int) or n < 1:
+        return None
+    return (n * (n + 1) * (2 * n + 1)) // 6
