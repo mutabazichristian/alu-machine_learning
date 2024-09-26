d6e6b0268c8eb7363a696eeb38239ebd65ada7ff
mutabazichristian <c.mutabazi@alustudent.com>
2024-09-26 15:52:07 +0200

    task 17

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -1,1 +1,21 @@
-5
+#!/usr/bin/env python3
+"""
+Function that calculates the integral of a polynomial
+"""
+
+
+def poly_integral(poly, C=0):
+    """
+    Arguments:
+    A list with the polynomial's coefficients, reverse order
+    The constant, if any
+
+    Returns:
+    A list of the integral's polynomial, reverse order
+    """
+    if not isinstance(poly, list) or len(poly) == 0:
+        return None
+    integral = [C]
+    for i in range(1, len(poly)):
+        integral.append(poly[i] / i)
+    return integral
