7b307483d25f49057782e7df588ca63f565d82fa
mutabazichristian <c.mutabazi@alustudent.com>
2024-10-08 16:11:04 +0200

    creating files and all

---
+++ math/probability/README.md
@@ -0,0 +1,2 @@
+WE learning some probability type things, na mean?
+Poisson, Guassian type shiii
---
+++ math/probability/poisson.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Class Poisson that represents a Poisson distribution
+"""
+
+
+class Poisson:
+    def __init__(self, data=None, lambtha=1.0):
+        self.data = data
+        self.lambtha = lambtha
