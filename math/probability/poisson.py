ef59c643bd3f7e084f8a89cbe8de24bc1d4e1264
mutabazichristian <c.mutabazi@alustudent.com>
2024-10-10 15:04:02 +0200

    creating file && attempting task 0

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
 """
-Class Poisson that represents a Poisson distribution
+Task 0:
+Create a class Poisson
 """
 
 
 class Poisson:
-    def __init__(self, data=None, lambtha=1.0):
-        self.data = data
-        self.lambtha = lambtha
+    """
+    Attributes:
+
+    """
+
+    def __init__(self, data=None, lambtha=1):
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contian multiple values")
+
+            self.lambtha = float(sum(data) / len(data))
