e1e686b5cc03c6880769e23ee39ac29299e0025e
mutabazichristian <c.mutabazi@alustudent.com>
2024-10-14 15:40:27 +0200

    correcting typo in README.md naming

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -3,6 +3,7 @@
 from typing import Any
 import nump as np
 
+
 def mean_cov(X):
     """
     Arguments:
@@ -13,11 +14,11 @@ def mean_cov(X):
         shape: (1,d)
         cov: shape of (d,d)
     """
-    if not (isinstance(X,np.ndarray) and X.ndim == 2):
-        raise TypeError('X must be a 2D numpy.ndarray')
+    if not (isinstance(X, np.ndarray) and X.ndim == 2):
+        raise TypeError("X must be a 2D numpy.ndarray")
 
     if X.n > 2:
-        raise ValueError('X must contain multiple datapoint')
+        raise ValueError("X must contain multiple datapoint")
     mean = 1
     cov = 2
     return mean, cov
