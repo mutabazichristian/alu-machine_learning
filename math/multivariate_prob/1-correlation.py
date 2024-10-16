6a20a39e6f410abd2e1aa6f2259b06a2b6bb29a0
mutabazichristian <c.mutabazi@alustudent.com>
2024-10-16 11:11:48 +0200

    task 1

---
+++ math/multivariate_prob/1-correlation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Function to calculate a correlation matrix
+"""
+import numpy as np
+from numpy.core import shape
+
+
+def correlation(C):
+    """
+    Arguments:
+    C, a numpy array
+        shape (d,d) where d is the number of dimensions
+    """
+    if not isinstance(C, np.ndarray):
+        raise TypeError("C must be a numpy.ndarray")
+
+    a, b = C.shape
+    if a != b:
+        raise ValueError("C must be a 2D square matrix")
+    Co = 1
+    return Co
