50a44fe7aa8ef88443f4e8695e6346246e20206e
mutabazichristian <c.mutabazi@alustudent.com>
2024-10-19 18:31:23 +0200

    task

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -22,16 +22,22 @@ def likelihood(x, n, P):
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
     if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0"
+        )
     if x > n:
         raise ValueError("x cannot be greater than n")
     if not isinstance(P, np.ndarray) or P.ndim != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
     if np.any((P < 0) | (P > 1)):
-        raise ValueError("All values in P must be in the range [0, 1]")
+        raise ValueError(
+            "All values in P must be in the range [0, 1]"
+        )
 
     # Calculate likelihood using binomial distribution
-    combo = np.math.factorial(n) / (np.math.factorial(x) * np.math.factorial(n - x))
+    combo = np.math.factorial(n) / (
+        np.math.factorial(x) * np.math.factorial(n - x)
+    )
     return combo * (P**x) * ((1 - P) ** (n - x))
 
 
