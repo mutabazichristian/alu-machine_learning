f521aa4b5f9fd6a3dd43c8b9b0643c8a5c76ea64
mutabazichristian <c.mutabazi@alustudent.com>
2025-01-17 11:37:09 +0200

    task 0

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Class that defines a single neuron perfoming binary classification
+"""
+import numpy as np
+
+class NeuronL:
+    def __init__(self,nx):
+        if not nx.isdigit():
+            raise TypeError("nx must be an integer")
+        if not nx>=1:
+            raise ValueError("nx must be a positive number")
+
+        self.W = np.random.randn(1, nx)
+        self.b = 0
+        self.A = 0
+
