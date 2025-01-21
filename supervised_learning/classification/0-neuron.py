8876d24b54c11cdde4f2cd78f88da81a692d13dd
mutabazichristian <c.mutabazi@alustudent.com>
2025-01-21 11:35:21 +0200

    still task 0

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -14,7 +14,7 @@ class Neuron:
     def __init__(self, nx):
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
-        if nx < 0:
+        if nx < 1:
             raise ValueError("nx must be a positive integer")
 
         self.W = np.random.randn(1, nx)
