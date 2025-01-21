cef8c1b7db9f7a6cf1697c844bd9f120f718c961
mutabazichristian <c.mutabazi@alustudent.com>
2025-01-21 11:57:25 +0200

    task 1

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -14,7 +14,7 @@ class Neuron:
         if nx < 1:
             raise ValueError("nx must be positive")
 
-        self.__W = np.random.randn(1, nx)
+        self.__W = np.random.random.normal(size=(1, nx))
         self.__b = 0
         self.__A = 0
 
