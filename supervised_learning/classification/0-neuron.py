b5cbdef4785f8576d034a2497ebdd4122654b972
mutabazichristian <c.mutabazi@alustudent.com>
2025-01-21 11:27:27 +0200

    corrected verification logic

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -14,8 +14,8 @@ class Neuron:
     def __init__(self, nx):
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
-        if nx <= 0:
-            raise ValueError("nx must be a positive number")
+        if nx < 0:
+            raise ValueError("nx must be a positive integer")
 
         self.W = np.random.randn(1, nx)
         self.b = 0
