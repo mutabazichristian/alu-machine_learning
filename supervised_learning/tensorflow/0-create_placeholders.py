d7e9e6bdb9b541e5cf01baf03eed551162581035
mutabazichristian <c.mutabazi@alustudent.com>
2025-01-31 10:40:55 +0200

    created a function that returns tensorflow placeholders for our classes

---
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Function that returns two placeholders for the neural network"""
+import tensorflow as tf
+
+
+def create_placeholder(nx, classes):
+    """
+    Args:
+        nx: the number of feauture columns in our data
+        classes: the number of classe in our classifier
+    Returns:
+        x: is the placeholder for the input data to the neural network
+        y: is the placeholder for the one-hot lables for the input data
+    """
+    x = tf.placeholder(dtype=tf.float32, shape=(None, nx), name="x")
+    y = tf.placeholder(dtype=tf.float32, shape=(None, classes), name="y")
+    return x, y
