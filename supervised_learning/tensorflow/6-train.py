66b7888d8aa14d4febfe9e59560ecb691da3817f
mutabazichristian <c.mutabazi@alustudent.com>
2025-01-31 13:36:47 +0200

    task 7

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -4,6 +4,19 @@ A function that build, trains and saves a neural network classifier
 """
 import tensorflow as tf
 
+create_placeholders = (
+    __import__("0-create_placeholders").create_placeholders,
+    forward_prop,
+) = (__import__("2-forward_prop").forward_prop, calculate_accuracy) = (
+    __import__("3-calculate_accuracy").calculate_accuracy,
+    calculate_loss,
+) = (
+    __import__("4-calculate_loss").calculate_loss,
+    create_train_op,
+) = __import__(
+    "5-create_train_op"
+).create_train_op
+
 
 def train(
     X_train,
