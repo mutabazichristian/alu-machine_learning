97074b438dc9b2e3793b15de2ec88f81fdcfdc46
mutabazichristian <c.mutabazi@alustudent.com>
2025-01-31 13:15:37 +0200

    created a function that calculates the accuracy of a prediction

--- supervised_learning/tensorflow/3-calculate_accuracy.py
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 """
-
+Write a function that calculates the accuracy of a prediction
 """
+import tensorflow as tf
+
+
+def calculate_accuracy(y, y_pred):
+    """
+    args
+        y: the actual data
+        y_pred: the prediction
+    returns
+        the accuracy of the predictoin
+    """
+    return tf.reduce_mean(
+        tf.cast(tf.equal(tf.argmax(y, 1), tf.argmax(y_pred)), tf.float32)
+    )
